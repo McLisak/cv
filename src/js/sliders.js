e3b76e2cd0c75e9f06ddc18c71e2919cc833d0b7
McLisak <McLisak@gmail.com>
2020-02-29 18:43:12 +0100

    slider adjustments

--- src/js/sliders.js
+++ src/js/sliders.js
@@ -1,5 +1,11 @@
-export const SLIDER_CLASS = '.slider';
-export const SLIDE_CLASS = '.slide';
+import scrollIntoView from 'smooth-scroll-into-view-if-needed';
+
+export const SLIDER_CLASS = 'slider';
+export const SLIDES_CONTAINER_CLASS = 'slides';
+export const SLIDE_CLASS = 'slide';
+export const CONTROL_CLASS = 'slider-control';
+export const CONTROL_PREV_CLASS = 'prev';
+export const CONTROL_NEXT_CLASS = 'next';
 export class Sliders {
   /**
    *
@@ -22,8 +28,13 @@ export class Sliders {
    */
   _parseSlides(sliders) {
     sliders.forEach((slider) => {
-      slider.slides = slider.getElementsByClassName(SLIDE_CLASS);
+      slider.Slider = {
+        slides: Array.from(slider.getElementsByClassName(SLIDE_CLASS)),
+        slidesContainer: slider.getElementsByClassName(SLIDES_CONTAINER_CLASS)[0],
+        activeSlide: null,
+      };
       this._createControls(slider);
+      this._observeSlides(slider);
     });
   }
 
@@ -35,12 +46,64 @@ export class Sliders {
     const prev = document.createElement('div');
     const next = document.createElement('div');
 
-    slider.prevEl = prev;
-    slider.nextEl = next;
+    slider.Slider.controls = { prev, next };
+    this._addControlListeners(slider);
+
     slider.appendChild(prev);
     slider.appendChild(next);
+    prev.classList.add(CONTROL_CLASS, CONTROL_PREV_CLASS);
+    next.classList.add(CONTROL_CLASS, CONTROL_NEXT_CLASS);
   }
-  _addControlListeners() {}
+
+  _addControlListeners(slider) {
+    const scrollOptions = {
+      behavior: 'smooth',
+      boundary: slider.Slider.slidesContainer,
+      scrollMode: 'always',
+      inline: 'center',
+      block: 'nearest',
+      duration: 30,
+    };
+    const prevClick = () => {
+      let prevIndex = slider.Slider.slides.indexOf(slider.Slider.activeSlide) - 1;
+      if (prevIndex < 0) {
+        prevIndex = 0;
+      }
+      scrollIntoView(slider.Slider.slides[prevIndex], scrollOptions);
+    };
+    const nextClick = () => {
+      let nextIndex = slider.Slider.slides.indexOf(slider.Slider.activeSlide) + 1;
+      if (nextIndex >= slider.Slider.slides.length - 1) {
+        nextIndex = slider.Slider.slides.length - 1;
+      }
+      scrollIntoView(slider.Slider.slides[nextIndex], scrollOptions);
+    };
+    slider.Slider.controls.prev.addEventListener('click', prevClick);
+    slider.Slider.controls.next.addEventListener('click', nextClick);
+  }
+
+  /**
+   * @param {HTMLElement} slider
+   */
+  _observeSlides(slider) {
+    slider.Slider.observer = new IntersectionObserver(
+      (entries) => this._observerCallback(entries, slider),
+      {
+        root: slider,
+        rootMargin: '-10px',
+        threshold: 1,
+      }
+    );
+    slider.Slider.slides.forEach((slide) => slider.Slider.observer.observe(slide));
+  }
+
+  _observerCallback(entries, slider) {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        slider.Slider.activeSlide = entry.target;
+      }
+    });
+  }
 }
 
 /**
